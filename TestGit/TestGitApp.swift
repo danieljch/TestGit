f4aafd1ab68b2e0eff55798c41b995bd1f7b2517
Daniel Callisaya <daniel_jch@hotmail.com>
2023-04-10 10:42:33 -0400

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
